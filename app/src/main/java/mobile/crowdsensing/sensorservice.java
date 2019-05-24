f1c74603ef2c9be2195062e35dee46f74e6806b7
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2019-05-24 12:55:25 +0530

    second commit

--- app/src/main/java/mobile/crowdsensing/sensorservice.java
+++ app/src/main/java/mobile/crowdsensing/sensorservice.java
@@ -14,6 +14,9 @@ import android.widget.Toast;
 public class sensorservice   extends Service implements SensorEventListener {
     private SensorManager mSensorManager;
     private Sensor mSensor;
+    private Sensor temp;
+    private Sensor humidity;
+    private Sensor light;
 
     @Nullable
 
@@ -25,6 +28,9 @@ public class sensorservice   extends Service implements SensorEventListener {
         mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
         // Get default sensor type
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        temp=mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        humidity=mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        light=mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
     }
 
     @Override
@@ -39,9 +45,9 @@ public class sensorservice   extends Service implements SensorEventListener {
 
         // Registering...
         mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        // Get default sensor type
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, temp, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, humidity, SensorManager.SENSOR_DELAY_NORMAL);
 
         return START_STICKY;
 
@@ -63,8 +69,13 @@ public class sensorservice   extends Service implements SensorEventListener {
             float x = values[0];
             float y = values[1];
             float z = values[2];
-            Toast.makeText(this,"Accelerometer service is started"+Float.toString(x),Toast.LENGTH_SHORT).show();
-            Log.i("kkkkkkkkkkkk", "MyClass.getView() — get item number " + values[2]);
+//            Toast.makeText(this,"Accelerometer service is started"+Float.toString(x),Toast.LENGTH_SHORT).show();
+            Log.i("kkkkkkkkkkkk", "Accelerometer reading" + values[2]);
+        }
+        if (light.getType() == Sensor.TYPE_LIGHT) {
+            float x =  sensorEvent.values[0];
+            Log.i("kkkkkkkkkkkk", " Ambient light level in SI lux units " + x);
+
         }
     }
 
